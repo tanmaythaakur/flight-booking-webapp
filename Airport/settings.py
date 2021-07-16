a6edadab0497270fbbd396cad5dd0e677be4543a
Thakur Tanmay Raj Singh <tanmaythakur2025@gmail.com>
2021-07-16 22:13:14 +0530

    Update settings.py

--- Airport/settings.py
+++ Airport/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-eien#435&*u77*vwd+=yyg@(dmoo%sk51m0(k@el4ol=y9dk-w"
+SECRET_KEY = "My_Secret_Key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
