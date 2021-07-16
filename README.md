10289e62673e6f57755e1b3a09cea2711d0f1f42
tanmaythaakur <tanmaythakur2025@gmail.com>
2021-07-16 20:35:34 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flight Booking Webapp using Django
+
+- A simple Django webapp that allows users to book a flight tickets from a list of available flights.
---
+++ flights/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Airportt, Flight, Passenger
+
+# Register your models here.
+
+admin.site.register(Airportt)
+admin.site.register(Flight)
+admin.site.register(Passenger)
